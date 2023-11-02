fb335c42e638b469e9b980a8444c74a3af1150e6
bobelbuho <bobelbuho>
2023-11-02 14:38:29 +0000

    First Commit from Locofy Builder

---
+++ src/pages/Frame.js
@@ -0,0 +1,760 @@
+import styled from "styled-components";
+
+const FooterChild = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  background-color: #fff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+const SocialMediaIcon = styled.img`
+  position: absolute;
+  height: 34.19%;
+  width: 11.81%;
+  top: 58.63%;
+  right: 1.09%;
+  bottom: 7.18%;
+  left: 87.1%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const Footer1 = styled.footer`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 1050px;
+  height: 117px;
+`;
+const Footer = styled.footer`
+  position: absolute;
+  top: 1362px;
+  left: 7px;
+  width: 1050px;
+  height: 117px;
+`;
+const SlideChild = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 1050px;
+  height: 528px;
+  object-fit: cover;
+`;
+const PictureIcon = styled.img`
+  position: absolute;
+  top: 5.6px;
+  left: 5px;
+  width: 982.6px;
+  height: 527.4px;
+`;
+const JeppeyTransformeLa = styled.span`
+  position: absolute;
+  top: 52px;
+  left: 536px;
+  display: inline-block;
+  width: 395px;
+  height: 294px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+const Slide = styled.div`
+  position: absolute;
+  top: 749px;
+  left: 0px;
+  width: 1050px;
+  height: 533px;
+  font-size: var(--font-size-21xl);
+`;
+const GrandeBarreDeRechercheChild = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 50px;
+  background-color: #d9d9d9;
+  width: 918px;
+  height: 70px;
+`;
+const IconStopCircle = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 16.39%;
+  top: 0%;
+  right: 83.61%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const Dpart = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  top: 0px;
+  left: 48.6px;
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 110px;
+  height: 28px;
+`;
+const Depart = styled.div`
+  position: absolute;
+  height: 39.44%;
+  width: 17.28%;
+  top: 29.58%;
+  right: 79.3%;
+  bottom: 30.99%;
+  left: 3.42%;
+`;
+const IconStopCircle1 = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 13.62%;
+  top: 0%;
+  right: 86.38%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const Destination1 = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  top: 0px;
+  left: 48.9px;
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 142px;
+  height: 28px;
+`;
+const Destination = styled.div`
+  position: absolute;
+  height: 39.44%;
+  width: 20.8%;
+  top: 29.58%;
+  right: 50.87%;
+  bottom: 30.99%;
+  left: 28.33%;
+`;
+const Date1 = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  height: 59.15%;
+  width: 21.57%;
+  top: 19.72%;
+  right: 22.55%;
+  bottom: 21.13%;
+  left: 55.88%;
+`;
+const Kg = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 37px;
+  display: inline-block;
+  width: 34px;
+  height: 37px;
+`;
+const Input = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 104px;
+  height: 37px;
+`;
+const IconArrowDown = styled.img`
+  position: absolute;
+  height: 43.24%;
+  width: 20.19%;
+  top: 29.73%;
+  right: 11.63%;
+  bottom: 27.03%;
+  left: 68.17%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const Kilos = styled.div`
+  position: absolute;
+  top: 18px;
+  left: 726px;
+  width: 104px;
+  height: 37px;
+`;
+const GrandeBarreDeRechercheItem = styled.div`
+  position: absolute;
+  top: 2.5px;
+  left: 204.2px;
+  border-right: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 1px;
+  height: 69px;
+`;
+const GrandeBarreDeRechercheInner = styled.div`
+  position: absolute;
+  top: -0.5px;
+  left: 831.5px;
+  border-right: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 1px;
+  height: 69px;
+`;
+const LineDiv = styled.div`
+  position: absolute;
+  top: 1.5px;
+  left: 725.5px;
+  border-right: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 1px;
+  height: 69px;
+`;
+const GrandeBarreDeRechercheChild1 = styled.div`
+  position: absolute;
+  top: -0.5px;
+  left: 467.5px;
+  border-right: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 1px;
+  height: 69px;
+`;
+const VectorIcon = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const IconArrowCircleRight = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  height: 89.01%;
+  width: 6.88%;
+  top: 2.82%;
+  right: 0.41%;
+  bottom: 8.17%;
+  left: 92.7%;
+`;
+const GrandeBarreDeRecherche = styled.div`
+  position: absolute;
+  top: 483px;
+  left: 91.8px;
+  width: 918px;
+  height: 71px;
+  @media screen and (max-width: 1200px) {
+    width: 887px;
+  }
+  @media screen and (max-width: 960px) {
+    display: none;
+    width: 887px;
+  }
+`;
+const PetiteBarreDeRechercheParent = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 356px;
+  height: 269px;
+`;
+const Kg0 = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  display: inline-block;
+  width: 63px;
+  height: 41px;
+`;
+const IconArrowDown1 = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  height: 47.32%;
+  width: 33.95%;
+  top: 31.22%;
+  right: 0%;
+  bottom: 21.46%;
+  left: 66.05%;
+`;
+const Kilos1 = styled.div`
+  position: absolute;
+  top: 163px;
+  left: 221px;
+  width: 97.8px;
+  height: 41px;
+`;
+const GroupChild = styled.div`
+  position: absolute;
+  top: 146.5px;
+  left: 198.5px;
+  border-right: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 1px;
+  height: 79px;
+`;
+const PetiteBarreDeRechercheParent1 = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 356px;
+  height: 269px;
+`;
+const Date2 = styled.div`
+  position: absolute;
+  top: 7.7px;
+  left: 54.9px;
+  font-size: var(--barre-de-recherche-size);
+  font-family: var(--barre-de-recherche);
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 69px;
+  height: 35px;
+`;
+const IconCalendar = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 30.83%;
+  top: 0%;
+  right: 69.17%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const Date11 = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  top: 153.3px;
+  left: 17.1px;
+  width: 123.9px;
+  height: 46px;
+`;
+const FrameChild = styled.img`
+  position: absolute;
+  top: 217px;
+  left: 1px;
+  border-radius: 20px;
+  width: 355px;
+  height: 56px;
+`;
+const Destination2 = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  top: 94px;
+  left: 69px;
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 197px;
+  height: 32px;
+`;
+const Dpart1 = styled.input`
+  border: none;
+  font-family: var(--barre-de-recherche);
+  font-size: var(--barre-de-recherche-size);
+  background-color: transparent;
+  position: absolute;
+  top: 25px;
+  left: 68px;
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 199px;
+  height: 33px;
+`;
+const Rechercher = styled.div`
+  position: absolute;
+  top: 225px;
+  left: 103px;
+  display: inline-block;
+  width: 128px;
+  height: 46px;
+`;
+const GroupParent = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 358px;
+  height: 275px;
+  overflow: hidden;
+`;
+const PetiteBarreDeNavigation = styled.div`
+  position: absolute;
+  top: 446px;
+  left: 361px;
+  box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+  width: 358px;
+  height: 275px;
+  display: none;
+  @media screen and (max-width: 960px) {
+    display: flex;
+  }
+  @media screen and (max-width: 420px) {
+    display: flex;
+  }
+`;
+const GroupItem = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 1043px;
+  height: 330px;
+  @media screen and (max-width: 1200px) {
+    width: 100%;
+  }
+`;
+const LaPlateformeDe = styled.h1`
+  margin: 0;
+  position: absolute;
+  top: 34px;
+  left: 562.5px;
+  font-size: inherit;
+  letter-spacing: 0.15em;
+  font-weight: 400;
+  font-family: inherit;
+  display: inline-block;
+  width: 343px;
+  height: 159px;
+  text-shadow: 5px 4px 5px rgba(0, 0, 0, 0.25);
+  -webkit-text-stroke: 1px #1e1d22;
+`;
+const GroupContainer = styled.div`
+  position: absolute;
+  top: 104px;
+  left: 25.5px;
+  width: 1043px;
+  height: 330px;
+  font-size: 32px;
+`;
+const Rechercher1 = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 40px;
+  font-size: var(--navbar-size);
+  font-weight: 200;
+  font-family: var(--navbar);
+  color: var(--color-black);
+  text-align: left;
+  display: inline-block;
+  width: 137px;
+  height: 28px;
+`;
+const VectorIcon2 = styled.img`
+  position: absolute;
+  height: 69.29%;
+  width: 15.31%;
+  top: 13.93%;
+  right: 84.69%;
+  bottom: 16.79%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const RechercherParent = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  top: 6px;
+  left: 0px;
+  width: 177px;
+  height: 28px;
+`;
+const PublierUnTrajet = styled.a`
+  text-decoration: none;
+  position: absolute;
+  top: 6px;
+  left: 46px;
+  font-weight: 200;
+  color: inherit;
+  display: inline-block;
+  width: 165px;
+  height: 28px;
+`;
+const VectorIcon3 = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 14.22%;
+  top: 0%;
+  right: 85.78%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const PublierUnTrajetParent = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 233px;
+  width: 211px;
+  height: 34px;
+`;
+const People = styled.div`
+  position: absolute;
+  top: 4px;
+  left: 36px;
+  width: 4px;
+  height: 18px;
+  overflow: hidden;
+`;
+const VectorIcon4 = styled.img`
+  position: absolute;
+  height: 31.94%;
+  width: 20.2%;
+  top: 19.35%;
+  right: 59.39%;
+  bottom: 48.71%;
+  left: 20.41%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const VectorIcon5 = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 63.27%;
+  top: 0%;
+  right: 36.73%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const VectorIcon6 = styled.img`
+  position: absolute;
+  height: 58.06%;
+  width: 8.16%;
+  top: 22.58%;
+  right: 0%;
+  bottom: 19.35%;
+  left: 91.84%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+`;
+const PeopleParent = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  top: 3px;
+  left: 573px;
+  width: 49px;
+  height: 31px;
+`;
+const Bouttons = styled.div`
+  position: absolute;
+  top: 25px;
+  left: 338px;
+  width: 622px;
+  height: 34px;
+`;
+const Logo1Icon = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 77px;
+  width: 74px;
+  height: 78px;
+  object-fit: cover;
+`;
+const Jeppey = styled.b`
+  position: absolute;
+  top: 18px;
+  left: 85px;
+  display: inline-block;
+  width: 168px;
+  height: 39px;
+`;
+const Logo = styled.a`
+  text-decoration: none;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 253px;
+  height: 78px;
+  font-size: var(--font-size-21xl);
+  color: inherit;
+  font-family: var(--font-istok-web);
+`;
+const BarreDeNavigation = styled.nav`
+  margin: 0;
+  position: absolute;
+  top: 0px;
+  left: 26px;
+  width: 960px;
+  height: 78px;
+  text-align: left;
+  font-size: var(--navbar-size);
+  color: var(--color-black);
+  font-family: var(--navbar);
+  @media screen and (max-width: 1200px) {
+    width: 100%;
+  }
+`;
+const PageAccueil = styled.div`
+  position: relative;
+  width: 1068.5px;
+  height: 1479px;
+`;
+const PageAccueilParentRoot = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+  font-size: var(--barre-de-recherche-size);
+  color: var(--color-black);
+  font-family: var(--barre-de-recherche);
+`;
+
+const Frame = () => {
+  return (
+    <PageAccueilParentRoot>
+      <PageAccueil>
+        <Footer id="footer">
+          <Footer1 id="footer">
+            <FooterChild />
+            <SocialMediaIcon alt="" src="/social-media.svg" />
+          </Footer1>
+        </Footer>
+        <Slide>
+          <SlideChild alt="" src="/rectangle-3@2x.png" />
+          <PictureIcon alt="" src="/picture.svg" />
+          <JeppeyTransformeLa id="title2">
+            Jeppey transforme la manière dont vous voyagez en créant des
+            connexions positives entre les voyageurs et en optimisant l'espace
+            disponible.
+          </JeppeyTransformeLa>
+        </Slide>
+        <GrandeBarreDeRecherche id="searchBarNav">
+          <GrandeBarreDeRechercheChild id="searchBox" />
+          <Depart>
+            <IconStopCircle alt="" src="/-icon-stop-circle.svg" />
+            <Dpart id="depart" placeholder="Départ" type="text" />
+          </Depart>
+          <Destination>
+            <IconStopCircle1 alt="" src="/-icon-stop-circle.svg" />
+            <Destination1
+              id="Destination"
+              placeholder="Destination"
+              type="text"
+            />
+          </Destination>
+          <Date1 id="date" type="date" />
+          <Kilos id="kilos">
+            <Kg>Kg</Kg>
+            <Input type="number" />
+            <IconArrowDown alt="" src="/-icon-arrow-down.svg" />
+          </Kilos>
+          <GrandeBarreDeRechercheItem />
+          <GrandeBarreDeRechercheInner />
+          <LineDiv />
+          <GrandeBarreDeRechercheChild1 />
+          <IconArrowCircleRight id="search">
+            <VectorIcon alt="" src="/vector.svg" />
+          </IconArrowCircleRight>
+        </GrandeBarreDeRecherche>
+        <PetiteBarreDeNavigation>
+          <GroupParent>
+            <PetiteBarreDeRechercheParent1>
+              <PetiteBarreDeRechercheParent
+                alt=""
+                src="/petite-barre-de-recherche.svg"
+              />
+              <Kilos1>
+                <Kg0>Kg 0</Kg0>
+                <IconArrowDown1>
+                  <VectorIcon alt="" src="/vector1.svg" />
+                </IconArrowDown1>
+              </Kilos1>
+              <GroupChild />
+            </PetiteBarreDeRechercheParent1>
+            <Date11 id="date">
+              <Date2>Date</Date2>
+              <IconCalendar alt="" src="/-icon-calendar.svg" />
+            </Date11>
+            <FrameChild alt="" src="/rectangle-6.svg" />
+            <Destination2 placeholder="Destination" type="text" />
+            <Dpart1 placeholder="Départ" type="text" />
+            <Rechercher>Rechercher</Rechercher>
+          </GroupParent>
+        </PetiteBarreDeNavigation>
+        <GroupContainer>
+          <GroupItem alt="" src="/group-5.svg" />
+          <LaPlateformeDe id="h1">
+            La plateforme de cobagage entre voyageur et expediteurs
+          </LaPlateformeDe>
+        </GroupContainer>
+        <BarreDeNavigation id="header">
+          <Bouttons>
+            <RechercherParent id="searchBar">
+              <Rechercher1>Rechercher</Rechercher1>
+              <VectorIcon2 alt="" src="/vector2.svg" />
+            </RechercherParent>
+            <PublierUnTrajetParent>
+              <PublierUnTrajet id="piblier">Publier un trajet</PublierUnTrajet>
+              <VectorIcon3 alt="" src="/vector3.svg" />
+            </PublierUnTrajetParent>
+            <PeopleParent id="profil">
+              <People />
+              <VectorIcon4 alt="" src="/vector4.svg" />
+              <VectorIcon5 alt="" src="/vector5.svg" />
+              <VectorIcon6 alt="" src="/vector6.svg" />
+            </PeopleParent>
+          </Bouttons>
+          <Logo id="logo">
+            <Logo1Icon alt="" src="/logo-1@2x.png" />
+            <Jeppey>Jeppey</Jeppey>
+          </Logo>
+        </BarreDeNavigation>
+      </PageAccueil>
+    </PageAccueilParentRoot>
+  );
+};
+
+export default Frame;
